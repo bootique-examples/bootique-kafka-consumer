dac770e178fcf43734232763bfd6816dfaa6e11f
Nikita Timofeev <stariy95@gmail.com>
2018-12-06 14:27:51 +0300

    Update to Bootique 1.0.RC1 and Bootique parent 0.13

--- src/main/java/io/bootique/example/kafka/consumer/App.java
+++ src/main/java/io/bootique/example/kafka/consumer/App.java
@@ -1,20 +1,11 @@
 package io.bootique.example.kafka.consumer;
 
 import com.google.inject.Binder;
-import com.google.inject.BindingAnnotation;
-import com.google.inject.Key;
 import com.google.inject.Module;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import io.bootique.BQCoreModule;
 import io.bootique.Bootique;
 import io.bootique.meta.application.OptionMetadata;
-import io.bootique.cli.Cli;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import java.util.logging.Level;
 
 /**
@@ -25,48 +16,30 @@ public class App implements Module {
     private static final String VERBOSE_OPT = "verbose";
 
     public static void main(String[] args) {
-        Bootique.app("--config=classpath:defaults.yml").args(args).autoLoadModules().module(App.class).run();
+        Bootique.app("--config=classpath:defaults.yml")
+                .args(args)
+                .autoLoadModules()
+                .module(App.class)
+                .exec()
+                .exit();
     }
 
     private static OptionMetadata verboseOption() {
         return OptionMetadata.builder(VERBOSE_OPT)
+                .valueOptionalWithDefault("INFO")
                 .description("If enabled, Kafka client will print extra debugging information to STDOUT.").build();
     }
 
     @Override
     public void configure(Binder binder) {
-        BQCoreModule.extend(binder).setDefaultCommand(KafkaConsumerCommand.class).addOption(verboseOption());
+        BQCoreModule.extend(binder)
+                .setDefaultCommand(KafkaConsumerCommand.class)
+                .addOption(verboseOption())
+                .mapConfigPath(VERBOSE_OPT, "log.loggers.org.apache.kafka.level")
+                .mapConfigPath(VERBOSE_OPT, "log.loggers.io.bootique.level");
 
-        // TODO: perhaps we'll invent less verbose API for conditional setting of log levels per
-        // https://github.com/bootique/bootique/issues/101
-        BQCoreModule.contributeLogLevels(binder).addBinding("org.apache.kafka").to(Key.get(Level.class, KafkaLogLevel.class));
-        BQCoreModule.contributeLogLevels(binder).addBinding("io.bootique").to(Key.get(Level.class, BootiqueLogLevel.class));
-    }
-
-
-    @KafkaLogLevel
-    @Provides
-    @Singleton
-    Level provideKafkaLogLevel(Cli cli) {
-        return cli.hasOption(VERBOSE_OPT) ? Level.INFO : Level.WARNING;
-    }
-
-    @BootiqueLogLevel
-    @Provides
-    @Singleton
-    Level provideBootiqueLogLevel(Cli cli) {
-        return cli.hasOption(VERBOSE_OPT) ? Level.INFO : Level.WARNING;
-    }
-
-    @Target({ElementType.PARAMETER, ElementType.FIELD, ElementType.METHOD})
-    @Retention(RetentionPolicy.RUNTIME)
-    @BindingAnnotation
-    @interface KafkaLogLevel {
-    }
-
-    @Target({ElementType.PARAMETER, ElementType.FIELD, ElementType.METHOD})
-    @Retention(RetentionPolicy.RUNTIME)
-    @BindingAnnotation
-    @interface BootiqueLogLevel {
+        BQCoreModule.extend(binder)
+                .setLogLevel("org.apache.kafka", Level.WARNING)
+                .setLogLevel("io.bootique", Level.WARNING);
     }
 }
--- src/main/java/io/bootique/example/kafka/consumer/KafkaConsumerCommand.java
+++ src/main/java/io/bootique/example/kafka/consumer/KafkaConsumerCommand.java
@@ -2,29 +2,26 @@ package io.bootique.example.kafka.consumer;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import io.bootique.kafka.client.consumer.KafkaConsumerFactory;
+import io.bootique.kafka.client.consumer.KafkaConsumerRunner;
 import io.bootique.meta.application.CommandMetadata;
 import io.bootique.meta.application.OptionMetadata;
 import io.bootique.cli.Cli;
 import io.bootique.command.CommandOutcome;
 import io.bootique.command.CommandWithMetadata;
-import io.bootique.kafka.client.KafkaClientFactory;
-import io.bootique.kafka.client.consumer.ConsumerConfig;
 import io.bootique.log.BootLogger;
+
 import io.bootique.shutdown.ShutdownManager;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.Collection;
 
 /**
  * Runs Kafka consumer. See
- * <a href="http://www.confluent.io/blog/tutorial-getting-started-with-the-new-apache-kafka-0.9-consumer-client">this article</a>
+ * <a href="http://www.confluent.io/blog/tutorial-getting-started-with-the-new-apache   -kafka-0.9-consumer-client">this article</a>
  * for background on the consumer API.
  */
 public class KafkaConsumerCommand extends CommandWithMetadata {
@@ -32,27 +29,20 @@ public class KafkaConsumerCommand extends CommandWithMetadata {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumerCommand.class);
 
     private static final String TOPIC_OPT = "topic";
-    private static final String REWIND_OPT = "rewind";
     private static final String BOOTSTRAP_SERVER_OPT = "bootstrap-server";
-
-    private static final String DEFAULT_CLUSTER_NAME = "default";
     private static final String DEFAULT_GROUP_NAME = "bootique-consumer";
 
-    private Provider<KafkaClientFactory> kafkaProvider;
+    private Provider<KafkaConsumerFactory> kafkaProvider;
     private BootLogger bootLogger;
-    private ShutdownManager shutdownManager;
 
     @Inject
-    public KafkaConsumerCommand(Provider<KafkaClientFactory> kafkaProvider, BootLogger bootLogger, ShutdownManager shutdownManager) {
-        super(CommandMetadata.builder("consumer").addOption(topicOption()).addOption(clusterOption()).addOption(rewindOption()));
+    public KafkaConsumerCommand(Provider<KafkaConsumerFactory> kafkaProvider, BootLogger bootLogger) {
+        super(CommandMetadata.builder("consumer")
+                .addOption(topicOption())
+                .addOption(clusterOption())
+        );
         this.kafkaProvider = kafkaProvider;
         this.bootLogger = bootLogger;
-        this.shutdownManager = shutdownManager;
-    }
-
-    private static OptionMetadata rewindOption() {
-        return OptionMetadata.builder(REWIND_OPT)
-                .description("Whether to rewind offsets for each consumed partition to the beginning of the queue.").build();
     }
 
     private static OptionMetadata topicOption() {
@@ -62,7 +52,7 @@ public class KafkaConsumerCommand extends CommandWithMetadata {
 
     private static OptionMetadata clusterOption() {
         return OptionMetadata.builder(BOOTSTRAP_SERVER_OPT).description("Single Kafka bootstrap server. " +
-                "Can be specified multiple times. Optional. " +
+                "Optional. " +
                 "If omitted, will be read from YAML or environment variable BQ_KAFKACLIENT_BOOTSTRAPSERVERS_DEFAULT.")
                 .valueRequired("host:port").build();
     }
@@ -75,34 +65,19 @@ public class KafkaConsumerCommand extends CommandWithMetadata {
             return CommandOutcome.failed(-1, "No --topic specified");
         }
 
-        ConsumerConfig<byte[], String> config = ConsumerConfig
-                .charValueConfig()
+        KafkaConsumerRunner<byte[], String> consumer = kafkaProvider.get()
+                .charValueConsumer()
                 .autoCommit(true)
                 .group(DEFAULT_GROUP_NAME)
-                .bootstrapServers(cli.optionStrings(BOOTSTRAP_SERVER_OPT))
-                .build();
-
-        boolean rewind = cli.hasOption(REWIND_OPT);
-        Consumer<byte[], String> consumer = kafkaProvider.get().createConsumer(DEFAULT_CLUSTER_NAME, config);
+                .cluster(cli.optionString(BOOTSTRAP_SERVER_OPT))
+                .topics(topics.toArray(new String[0]))
+                .pollInterval(Duration.ofSeconds(1))
+                .create();
 
         try {
-
-            shutdownManager.addShutdownHook(() -> {
-                consumer.wakeup();
-                // give consumer time to wakeup and stop...
-                Thread.sleep(500);
-            });
-
             LOGGER.info("Will consume topics: " + topics);
-
-            consumer.subscribe(topics, new RebalanceListener(consumer, rewind));
-
-            while (true) {
-                ConsumerRecords<byte[], String> records = consumer.poll(1000);
-                for (ConsumerRecord<byte[], String> r : records) {
-                    bootLogger.stdout(r.topic() + "_" + r.partition() + "_" + r.offset() + ": " + r.value());
-                }
-            }
+            consumer.forEach(record
+                    -> bootLogger.stdout(record.topic() + "_" + record.partition() + "_" + record.offset() + ": " + record.value()));
         } catch (WakeupException e) {
             // expected on shutdown..
             LOGGER.info("Stopping consumer...");
@@ -111,33 +86,5 @@ public class KafkaConsumerCommand extends CommandWithMetadata {
         }
 
         return CommandOutcome.succeeded();
-    }
-
-    class RebalanceListener implements ConsumerRebalanceListener {
-
-        private Consumer<byte[], String> consumer;
-        private boolean rewind;
-
-        public RebalanceListener(Consumer<byte[], String> consumer, boolean rewind) {
-            this.consumer = consumer;
-            this.rewind = rewind;
-        }
-
-        @Override
-        public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
-            LOGGER.info("Partitions revoked: " + partitions);
-        }
-
-        @Override
-        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
-            LOGGER.info("Partitions assigned: " + partitions);
-
-            // TODO: if rebalancing occurs because another consumer joins the group, this event is fired for the
-            // already consumed partition. We probably should not jump to the start again
-            if (rewind) {
-                LOGGER.info("Will rewind " + partitions);
-                consumer.seekToBeginning(partitions);
-            }
-        }
     }
 }
